f72c625a010b90b85696e71d1e7fd85ee404b4a4
Piyush Pamnani <piyushpamnani46@gmail.com>
2023-08-06 19:46:02 +0530

    Edit and Delete Prompt

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,20 +1,53 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
-  const handleEdit = () => {};
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [promptsData, setPromptsData] = useState([]);
 
-  const handleDelete = async () => {};
+  const handleEdit = (prompt) => {
+    router.push(`/update-prompt?id=${prompt._id}`);
+  };
+
+  const handleDelete = async (prompt) => {
+    const hasConfirmed = confirm("Are you sure you want to delete the Prompt?");
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${prompt._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPrompt = promptsData.filter((p) => p._id !== prompt._id);
+
+        setPromptsData(filteredPrompt);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPromptsData(data);
+    };
+
+    if (session?.user.id) fetchPost();
+  }, []);
 
   return (
     <Profile
       name="My"
       desc="Welcome to your personalized profile page"
-      data={[]}
+      data={promptsData}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
